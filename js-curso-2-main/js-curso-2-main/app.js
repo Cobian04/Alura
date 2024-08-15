1db6403cf4930b3331aca59949a907b6077eabff
Cobian04 <jancobian19@gmail.com>
2024-08-14 18:28:05 -0600

    Se completaron los desafios de practica correctamente

--- js-curso-2-main/js-curso-2-main/app.js
+++ js-curso-2-main/js-curso-2-main/app.js
@@ -1,1 +1,22 @@
+let titulo = document.querySelector("h1")
+titulo.innerHTML = "Aqui se cambia el titulo"
 
+function botonClickeado(){
+    alert("Este boton fue clickeado")
+}
+
+function botonPromptClickeado(){
+    let ciudad = prompt("Por favor, escribe una ciudad de brasil")
+    console.log("Estuve en " + ciudad + " Y me acorde de ti")
+}
+
+function botonAlerta(){
+    alert("Yo amo a JS")
+}
+
+function botonSuma(){
+    let num1 = prompt("Por favor teclea tu numero")
+    let num2 = prompt("Por favor teclea tu segundo numero")
+    let resultado = num1+num2
+    console.log(resultado)
+}
